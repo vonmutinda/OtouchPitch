9cb5be6ee35f371f192078da3c565ac3cc3990e3
vonmutinda <maxwellwambua@gmail.com>
2018-09-07 16:07:51 +0300

    macro for looping through pitches

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 
 from flask_login import LoginManager
+from flask_mail import Mail
 
 from config import config_options
 
@@ -10,7 +11,7 @@ from config import config_options
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 login_manager = LoginManager()
-
+mail = Mail()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -23,10 +24,15 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
 
      # *** Registering the blueprint ***
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    # Registering auth blueprint
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix='/authenticate')    
 
 
     return app
---
+++ start.sh
@@ -0,0 +1,6 @@
+export SECRET_KEY=bxxfcxa43xf7xd9xc6xefxf8c
+
+export MAIL_USERNAME=maxwellwambua@gmail.com
+export MAIL_PASSWORD=vonMUTINDA01
+
+python manage.py server
