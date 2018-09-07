022b734164982dffbeac8c05c8dd159891b0a14d
vonmutinda <maxwellwambua@gmail.com>
2018-09-08 01:43:01 +0300

    README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+
+## Specifications
+Get the specs [here](https://github.com/vonmutinda/onetouch/blob/master/specs.md)
--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+# basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY')
     # SQLALCHEMY_DATABASE_URI='sqlite:///C:\\Users\\Username\\AppData\\Roaming\\Appname\\mydatabase.db'
-    SQLALCHEMY_DATABASE_URI =  os.environ.get('DATABASE_URL') or \
-        'postgresql+psycopg2:///' + os.path.join(basedir, 'minuteone.db')
+    # SQLALCHEMY_DATABASE_URI =  os.environ.get('DATABASE_URL') or \
+    #     'postgresql+psycopg2:///' + os.path.join(basedir, 'minuteone.db')
 
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://vonmutinda:von@localhost/minuteone'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:von@localhost/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
