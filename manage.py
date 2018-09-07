7cc5a104b4a86a2135da552d3ab542d6871ece37
vonmutinda <maxwellwambua@gmail.com>
2018-09-07 08:16:28 +0300

    Routing to index.html

---
+++ manage.py
@@ -0,0 +1,27 @@
+from app import create_app 
+from flask_script import Manager ,Server
+
+from flask_migrate import Migrate , MigrateCommand 
+
+#create app instance
+app = create_app('development')
+
+manager = Manager(app)
+
+manager.add_command('server',Server)
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+@manager.shell
+def make_shell_context():
+    return dict( app = app )
+
+
+
+if __name__ == '__main__':
+    manager.run()
