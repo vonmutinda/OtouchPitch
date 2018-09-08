6bfa2f6b4fed16d47be78b361ee293563817d03c
vonmutinda <maxwellwambua@gmail.com>
2018-09-08 14:34:02 +0300

    styling navbar

--- app/main/views.py
+++ app/main/views.py
@@ -10,7 +10,33 @@ def index():
 
     return render_template('index.html')
 
+
+'''
+This route will navigate to marriage proposal pitches only .
+
+Will query the database for pitches from proposal category then pass them to macro for looping
+'''
 @main.route('/pitches/proposal')
 def proposal():
+
+    return render_template('pitches.html')
+
+
+'''
+A route for pick-up lines page 
+'''
+@main.route('/pitch/pick-up')
+def pick_up():
+
+
+    return render_template('pitches.html')
+
+
+'''
+A route for pitches of how to best describe yourself in only a minute . 
+'''
+@main.route('/pitch/describe-yourself')
+def about_you():
+
 
     return render_template('pitches.html')
--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from datetime import datetime
 from werkzeug.security import generate_password_hash , check_password_hash
 from flask_login import UserMixin 
 from . import login_manager
@@ -9,7 +10,7 @@ class User(UserMixin , db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     email = db.Column(db.String(255) , unique = True , index = True )
-    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
     pass_secure = db.Column(db.String(255))
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
@@ -32,3 +33,36 @@ class User(UserMixin , db.Model):
 @login_manager.user_loader 
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+
+class Pitches(db.Model):
+
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer,primary_key = True)
+    pitch_id = db.Column(db.Integer)
+    movie_title = db.Column(db.String)
+    pitch = db.Column(db.String)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    category_id = db.Column(db.Integer , db.ForeignKey("categories.id"))
+
+    users = db.relationship('User',backref = 'pitch',lazy="dynamic")
+
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_pitches(cls,id):
+        pitches = Pitches.query.filter_by(pitch_id = id).all()
+
+        return pitches
+
+
+class Category(db.Model):
+    
+    __tablename__ = 'categories'
+
+    id = db.Column (db.Integer , primary_key = True)
+    category = db.Column (db.String)
